45b21d857587c99443664d1eb93c7e3dd0de276e
Andy Boggs <ajacobboggs@gmail.com>
2018-02-26 14:27:44 -0800

    add additonal select boxes & else if branches

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,20 @@
 $(document).ready(function() {
   $("form#choose").submit(function(event) {
     var beaches = $("select#beaches").val();
+    var sunshine = $("select#sunshine").val();
+    var seafood = $("select#seafood").val();
+    var spanish = $("select#spanish").val();
+    var money = $("select#money").val();
 
-
-    if (beaches === "Yes") {
+    if (beaches === "Yes" && sunshine === "Yes" && seafood === "Yes" && spanish === "No" && money === "Yes") {
       $("#Jamaica").show();
-  } else if (beaches === "No") {
-      $("#Oregon").show();
-  }
-
+    } else if (beaches === "Yes" && sunshine === "Yes" && seafood === "Yes" && spanish === "Yes" && money === "No") {
+        $("#Peru").show();
+    } else if (beaches === "No" && sunshine === "No" && seafood === "Yes" && spanish === "No" && money === "No") {
+        $("#Oregon").show();
+    } else {
+        $("#NoVacation").show();
+    }
 
     event.preventDefault();
  });
